23d33c54876b5ba9d6e7f5e31c1482b57cacafe0
Ipsita Mishra <ipsitamishra16893@gmail.com>
2024-03-30 00:01:43 +0100

    initial structure

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// Service worker logic
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-pwa-cache-v1').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/app.js'
+          // Add other static assets to cache here
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
